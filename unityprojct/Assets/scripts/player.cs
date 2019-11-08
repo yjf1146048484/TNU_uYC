cc7af345feeb5712a448abcd2188b0c93bf420ab
yjf1146048484 <56706517+yjf1146048484@users.noreply.github.com>
2019-11-08 11:59:37 +0800

    jiaobeng

---
+++ unityprojct/Assets/scripts/player.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour
+{
+    [Header(" ")][Range(0f,100f)]
+    public float speed = 3.5f;
+    [Header(" "),Range(100,2000)]
+    public int junp = 300;
+    []
+    public bool isGround = false;
+    public string name = "uYC";
+}
